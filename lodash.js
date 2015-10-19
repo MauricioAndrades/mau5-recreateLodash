3d7dcf1e29154bef7b65bfc6742211418ad1407e
Cho S. Kim <choskim.me@gmail.com>
2015-10-19 11:37:33 -0700

    Add lodash.js

---
+++ lodash.js
@@ -0,0 +1,82 @@
+var _ = {};
+
+/**************************************
+ *************** ARRAYS ***************
+ **************************************/
+
+ // Returns the first element of an array
+_.first = function() {
+	// Place your solution here
+};
+
+ // returns the first n number of elements in an array.
+_.take = function() {
+	// Place your solution here
+};
+
+// Returns the last element of an array
+_.last = function() {
+	// Place your solution here
+};
+
+// returns the last n number of elements in an array.
+_.takeRight = function() {
+	// Place your solution here
+};
+
+_.compact = function() {
+  // Place your solution here	
+};
+
+_.difference = function() {
+	// Place your solution here	
+};
+
+_.uniq = function() {
+	// Place your solution here	
+};
+
+_.max = function() {
+	// Place your solution here	
+};
+
+_.min = function() {
+	// Place your solution here	
+};
+
+_.indexOf = function() {
+	// Place your solution here	
+};
+
+/*************** BONUS ***************/
+_.shuffle = function() {
+	// Place your solution here	
+};
+
+/**************************************
+************* COLLECTIONS *************
+**************************************/
+
+_.size = function() {
+	// Place your solution here	
+};
+
+_.forEach = function() {
+	// Place your solution here	
+};
+
+_.filter = function() {
+	// Place your solution here	
+};
+
+_.reject = function() {
+	// Place your solution here	
+};
+
+/*************** BONUS ***************/
+_.sample = function() {
+	// Place your solution here	
+};
+
+
+module.exports = _;
