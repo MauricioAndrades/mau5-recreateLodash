7a16e8dfdb3389f44d651bfca8fb6ea7a2883300
Cho S. Kim <choskim.me@gmail.com>
2015-10-29 03:52:52 -0700

    Add missing comments

--- lodash.js
+++ lodash.js
@@ -24,31 +24,46 @@ _.takeRight = function() {
 	// Place your solution here
 };
 
+// Creates a new array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+// Example:
+// _.compact([0, 1, false, 2, '', 3]);
+// → [1, 2, 3]
 _.compact = function() {
   // Place your solution here	
 };
 
+// Creates an array of unique array values not included in the other provided arrays
+// Example:
+// _.difference([1, 2, 3], [4, 2]);
+// → [1, 3]
 _.difference = function() {
 	// Place your solution here	
 };
 
+// Create a duplicate free version of the array
+// _.uniq([2, 1, 2]);
+// → [2, 1]
 _.uniq = function() {
 	// Place your solution here	
 };
 
+// Returns maximum value in an array
 _.max = function() {
 	// Place your solution here	
 };
 
+// Returns min value in an array
 _.min = function() {
 	// Place your solution here	
 };
 
+// Returns index of matched value, else return -1
 _.indexOf = function() {
 	// Place your solution here	
 };
 
 /*************** BONUS ***************/
+// Retuns a new shuffled array
 _.shuffle = function() {
 	// Place your solution here	
 };
@@ -56,24 +71,28 @@ _.shuffle = function() {
 /**************************************
 ************* COLLECTIONS *************
 **************************************/
-
+// Returns the length of the collection
 _.size = function() {
 	// Place your solution here	
 };
 
+// Iterates on each item of the collection and then returns the original collection
 _.forEach = function() {
 	// Place your solution here	
 };
 
+// Returns a new array with the filtered items
 _.filter = function() {
 	// Place your solution here	
 };
 
+// Returns a new array with the unfiltered items
 _.reject = function() {
 	// Place your solution here	
 };
 
 /*************** BONUS ***************/
+// Returns n random items
 _.sample = function() {
 	// Place your solution here	
 };
